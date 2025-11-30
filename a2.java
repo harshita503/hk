959da2d108b49eccf0a2dfc85ea934dd94d18e30
Harshita Kumari <kumariharshita426@gmail.com>
2025-11-30 11:45:19 +0530

    nested loop

---
+++ a2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class a2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("pattern");
+        int i,j;
+        for ( i =5; i>=1; i--) {
+            for(j=1;j<=i;j++){
+                System.out.print("*");
+
+            }
+            System.out.println();
+            
+
+
+            
+        }
+
+
+    }
+    
+}
